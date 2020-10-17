9483d820503fa33f73ec0eab6c98981b36bba3da
Jordy <jordy@nefkon.com>
2020-10-17 17:44:27 +0200

    activation email

---
+++ app/Notifications/AccountActivation.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+use App\User;
+
+class AccountActivation extends Notification
+{
+    use Queueable;
+
+    /**
+     * User Object
+     *
+     * @var App\User
+     */
+    public $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $vueApp = 'https://alvote.nl';
+        $url = url($vueApp . '/confirm/account/' . $this->user->activation_token);
+
+        return (new MailMessage)
+            ->from('no-reply@alvote.nl')
+            ->subject('Account activation')
+            ->markdown('email.account.activation', [
+                'url' => $url,
+                'name' => $this->user->email,
+            ]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ resources/views/email/account/activation.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+# Hi {{$name}},
+
+Thanks for signing up for ALVote.nl. 
+
+Before loggin into the app you'll need to activate your account.
+
+Follow the link: 
+{{ url }}
+
+If you have any questions we will be happy to help you. You can contact us on info@alvote.nl.
+
+Regards,<br>
+Team Alvote
+@endcomponent
